2e47039d8a550396bd74eaa08bda5e4e56c3e471
vetu11 <vetu@protonmail.com>
2024-02-21 14:41:59 +0100

    Item

--- src/main/java/dynwr/DynWR.java
+++ src/main/java/dynwr/DynWR.java
@@ -10,6 +10,7 @@ public class DynWR implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("dynwr");
+    public static final Item CUSTOM_ITEM = new Item(new FabricItemSettings());
 
 	@Override
 	public void onInitialize() {
@@ -17,6 +18,7 @@ public class DynWR implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("TU pute NEIDRE caodre");
+        Registry.register(Registries.ITEM, new Identifier("tutorial", "custom_item"), CUSTOM_ITEM);
 	}
 }
